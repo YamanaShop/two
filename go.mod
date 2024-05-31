9d70ecdd0bd404e284aa3406c495e89fef28c943
YamanaShop <159576695+YamanaShop@users.noreply.github.com>
2024-05-31 15:20:09 +0200

    sec

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/YamanaShop/one
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var y string
+
+const w string = "unknown"
+
+func main() {
+	v := "short"
+	y = "vay"
+	fmt.Println(y, w, v)
+}
